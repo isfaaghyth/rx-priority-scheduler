7f7b6dde5d308363cef11c8d73ef38ac366f9891
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-12-15 17:42:52 +0700

    create test class

---
+++ rxpriorityscheduler/src/test/java/isfaaghyth/app/rxpriorityscheduler/PrioritySchedulerTest.kt
@@ -0,0 +1,27 @@
+package isfaaghyth.app.rxpriorityscheduler
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+import java.util.ArrayList
+import java.util.Collections
+import java.util.HashSet
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+import io.reactivex.Observable
+import io.reactivex.functions.Consumer
+import io.reactivex.Scheduler
+
+import org.junit.Assert.fail
+import org.junit.Assert.assertEquals
+
+/**
+ * Created by isfaaghyth on 15/12/18.
+ * github: @isfaaghyth
+ */
+@RunWith(JUnit4::class)
+class PrioritySchedulerTest {
+
+}
